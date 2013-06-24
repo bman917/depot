1cf4c59d45236cc53b6000d304da9fa26ce0800c
Ubuntu <ubuntu@ip-172-31-5-70.ap-southeast-1.compute.internal>
2013-06-24 10:21:55 +0000

    xxx

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '678bb2524a09a9a96d0b9fe1b7fb2a003f09c45754b768c76a25aa8dc9999efd763006b8e8ce7861e43a1dea7adf604dbd5ef9a7e2f7dd3a88eddf66d2f1f5c0'
